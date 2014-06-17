90ec2a95d1a7bf0c76b24998c602cf37fce3a4e9
Joe Gibson <gibsjose@mail.gvsu.edu>
2014-06-16 20:11:03 -0400

    Initial Commit

---
+++ Starfield/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Starfield
+//
+//  Created by Joe Gibson on 6/16/14.
+//  Copyright gibsjose 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Starfield/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Starfield
+//
+//  Created by Joe Gibson on 6/16/14.
+//  Copyright gibsjose 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
